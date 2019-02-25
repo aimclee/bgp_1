9c9e5d583c9122c6664bf7faef5854b90283f8c7
MCLEE <dhy04057@gmail.com>
2019-02-25 17:39:51 +0900

    2nd commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blog_project_1.wsgi --log-file -
--- blog_project_1/settings.py
+++ blog_project_1/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +140,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 # URL 설계방식 : 우리홈페이지 이름/media/파일에 해당하는 내용
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
